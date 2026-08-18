480de5db9d324db6ba7f5afac17bc8d1ef6396cc
Büşra Seven <busras3v3n@proton.me>
2026-08-18 12:25:21 +0300

    ex00

--- ex00/easyfind.tpp
+++ ex00/easyfind.tpp
@@ -6,13 +6,15 @@
 /*   By: busseven <busseven@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/16 15:04:12 by busseven          #+#    #+#             */
-/*   Updated: 2026/08/18 10:44:57 by busseven         ###   ########.fr       */
+/*   Updated: 2026/08/18 12:24:23 by busseven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <algorithm>
 #include <iterator>
+#include <iostream>
 #include "exceptions.hpp"
+
 
 template< typename T >
 typename T::iterator	easyfind(T &cont, int i)
@@ -23,4 +25,25 @@ typename T::iterator	easyfind(T &cont, int i)
 	if(iter == cont.end())
 		throw NotFoundException();
 	return(iter);
+}
+
+template< typename T >
+void run_container_test(const std::string& testname, int fill_limit, int step, int target1, int target2)
+{
+	std::cout << testname << std::endl;
+	T cont;
+	for(int i = 0; i < fill_limit; i+=step)
+		cont.push_back(i);
+	try
+	{
+		std::cout << "Finding " << target1 << std::endl;
+		typename T::iterator found = easyfind(cont, target1);
+		std::cout << "Found " << *found << std::endl;
+		std::cout << "Finding " << target2 << std::endl;
+		found = easyfind(cont, target2);
+	}
+	catch(std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 }
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: busseven <busseven@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/16 18:21:09 by busseven          #+#    #+#             */
-/*   Updated: 2026/08/18 11:52:05 by busseven         ###   ########.fr       */
+/*   Updated: 2026/08/18 12:25:02 by busseven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,44 +15,9 @@
 #include <stack>
 #include <iostream>
 
-void announce(int i)
-{
-	std::cout << "Finding " << i << std::endl;
-}
-
 int main(void)
 {
-	std::cout << "VECTOR TESTS" << std::endl;
-	std::vector<int> v;
-	for(int i = 0; i < 8; i++)
-		v.push_back(i);
-	try
-	{
-		announce(3);
-		std::vector<int>::iterator found = easyfind(v, 3);
-		std::cout << "Found " << *found << std::endl;
-		announce(42);
-		found = easyfind(v, 42);
-	}
-	catch(std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
+	run_container_test<std::vector<int> >("VECTOR TESTS", 8, 1, 3, 42);
 	std::cout << std::endl;
-	std::cout << "DEQUE TESTS" << std::endl;
-	std::deque<int> d;
-	for(int i = 0; i < 80; i+=10)
-		d.push_back(i);
-	try
-	{
-		announce(30);
-		std::deque<int>::iterator found2 = easyfind(d, 30);
-		std::cout << "Found " << *found2 << std::endl;
-		announce(80);
-		found2 = easyfind(d, 80);		
-	}
-	catch(std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
+	run_container_test<std::deque<int> >("DEQUE TESTS", 80, 10, 30, 80);
 }
