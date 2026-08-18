bcdd50fc18bb5201d435a187753ce2703c029821
Büşra Seven <busras3v3n@proton.me>
2026-08-18 11:52:57 +0300

    ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: busseven <busseven@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/16 18:21:09 by busseven          #+#    #+#             */
-/*   Updated: 2026/08/18 11:38:35 by busseven         ###   ########.fr       */
+/*   Updated: 2026/08/18 11:52:05 by busseven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,23 @@ int main(void)
 		std::cout << "Found " << *found << std::endl;
 		announce(42);
 		found = easyfind(v, 42);
+	}
+	catch(std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << std::endl;
+	std::cout << "DEQUE TESTS" << std::endl;
+	std::deque<int> d;
+	for(int i = 0; i < 80; i+=10)
+		d.push_back(i);
+	try
+	{
+		announce(30);
+		std::deque<int>::iterator found2 = easyfind(d, 30);
+		std::cout << "Found " << *found2 << std::endl;
+		announce(80);
+		found2 = easyfind(d, 80);		
 	}
 	catch(std::exception &e)
 	{
