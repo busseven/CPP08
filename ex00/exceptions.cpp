f663e42ef5e19099c8ac60e8ebcb3a6a46ea02b3
Büşra Seven <busras3v3n@proton.me>
2026-08-18 11:20:05 +0300

    ex00

--- ex00/exceptions.cpp
+++ ex00/exceptions.cpp
@@ -6,13 +6,11 @@
 /*   By: busseven <busseven@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/16 18:16:11 by busseven          #+#    #+#             */
-/*   Updated: 2026/08/18 11:18:48 by busseven         ###   ########.fr       */
+/*   Updated: 2026/08/18 11:20:01 by busseven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exceptions.hpp"
-#include <string>
-#include <strstream>
 
 const char *NotFoundException::what() const throw()
 {
