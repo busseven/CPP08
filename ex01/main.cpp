2fcd8c87f32f51546ca17b08741405cb62494cb8
Büşra Seven <busras3v3n@proton.me>
2026-08-20 12:58:10 +0300

    ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,18 +6,26 @@
 /*   By: busseven <busseven@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/20 09:46:37 by busseven          #+#    #+#             */
-/*   Updated: 2026/08/20 12:53:50 by busseven         ###   ########.fr       */
+/*   Updated: 2026/08/20 12:57:57 by busseven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
+
+void test_shortestSpan()
+{
+	Span sp = Span(10);
+	sp.addNumber(11);
+	sp.shortestSpan();
+}
 
 void test_addNumber()
 {
 	Span sp = Span(3);
 	sp.addNumber(-21);
 	sp.addNumber(0);
-	sp.addNumber(7);	
+	sp.addNumber(7);
+	sp.addNumber(5);
 }
 
 int main()
@@ -31,7 +39,14 @@ int main()
 	sp.addNumber(11);
 	std::cout << sp.shortestSpan() << std::endl;
 	std::cout << sp.longestSpan() << std::endl;
-
-	test_addNumber();
+	try
+	{
+		test_addNumber();
+		test_shortestSpan();
+	}
+	catch(std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 	return (0);
 }
