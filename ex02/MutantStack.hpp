dec9a6939a34dbc36ee273f03818d92f3e49bf1b
B mac <busras3v3n@proton.me>
2026-09-07 14:20:20 +0300

    ex02

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -12,12 +12,22 @@ class MutantStack : public std::stack<T, Container>
 		// Constructors
 		MutantStack();
 		MutantStack(const MutantStack &copy);
-		
 		// Destructor
 		~MutantStack();
-		
 		// Operators
 		MutantStack & operator=(const MutantStack &assign);
+
+		typedef typename Container::iterator iterator;
+
+		iterator begin() {return this->c.begin()};
+		iterator end() {return this->c.end()};
+		const_iterator begin() const {return this->c.begin()};
+		const_iterator end() const {return this->c.end()};
+		reverse_iterator rbegin() {return this->c.rbegin()};
+		reverse_iterator rend() {return this->c.rend()};
+		const_reverse_iterator rbegin() const {return this->c.rbegin()};
+		const_reverse_iterator rend() const {return this->c.rend()};
+
 		
 	private:
 		
