d78d5a55862b7a14f3bd2f70c576e1f1bb73177a
B mac <busras3v3n@proton.me>
2026-09-07 14:23:44 +0300

    ex02

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -18,15 +18,18 @@ class MutantStack : public std::stack<T, Container>
 		MutantStack & operator=(const MutantStack &assign);
 
 		typedef typename Container::iterator iterator;
+		typedef typename Container::const_iterator	const_iterator;
+		typedef typename Container::reverse_iterator	reverse_iterator;
+		typedef typename Container::const_reverse_iterator	const_reverse_iterator;
 
-		iterator begin() {return this->c.begin()};
-		iterator end() {return this->c.end()};
-		const_iterator begin() const {return this->c.begin()};
-		const_iterator end() const {return this->c.end()};
-		reverse_iterator rbegin() {return this->c.rbegin()};
-		reverse_iterator rend() {return this->c.rend()};
-		const_reverse_iterator rbegin() const {return this->c.rbegin()};
-		const_reverse_iterator rend() const {return this->c.rend()};
+		iterator begin() {return this->c.begin();}
+		iterator end() {return this->c.end();}
+		const_iterator begin() const {return this->c.begin();}
+		const_iterator end() const {return this->c.end();}
+		reverse_iterator rbegin() {return this->c.rbegin();}
+		reverse_iterator rend() {return this->c.rend();}
+		const_reverse_iterator rbegin() const {return this->c.rbegin();}
+		const_reverse_iterator rend() const {return this->c.rend();}
 
 		
 	private:
@@ -52,9 +55,9 @@ MutantStack<T, Container>::~MutantStack()
 }
 
 template <typename T, typename Container>
-MutantStack<T, Container>& MutantStack<T, Container>::operator=(const MutantStack &copy)
+MutantStack<T, Container>& MutantStack<T, Container>::operator=(const MutantStack &assign)
 {
-	std::stack<T, Container>::operator=(copy);
+	std::stack<T, Container>::operator=(assign);
 	return *this;
 }
 
