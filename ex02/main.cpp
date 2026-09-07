35b6133f6e227a74180eaebe25ce5f31425a494e
B mac <busras3v3n@proton.me>
2026-09-07 15:48:09 +0300

    ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,64 @@
 /*   By: busseven <busseven@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/07 12:40:52 by busseven          #+#    #+#             */
-/*   Updated: 2026/09/07 13:35:43 by busseven         ###   ########.fr       */
+/*   Updated: 2026/09/07 15:47:49 by busseven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "MutantStack.hpp"
+#include <list>
+#include <deque>
+#include <vector>
+
+int main()
+{
+    MutantStack<int> mstack;
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    mstack.push(0);
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+    std::cout << *it << std::endl;
+    ++it;
+    }
+    std::stack<int> s(mstack);
+
+    std::cout << std::endl;
+    std::cout << "LIST TESTS" << std::endl;
+    std::list<int> mlist;
+    mlist.push_back(5);
+    mlist.push_back(17);
+    std::cout << mlist.back() << std::endl;
+    mlist.pop_back();
+    std::cout << mlist.size() << std::endl;
+
+    mlist.push_back(3);
+    mlist.push_back(5);
+    mlist.push_back(737);
+    mlist.push_back(0);
+
+    std::list<int>::iterator lit = mlist.begin();
+    std::list<int>::iterator lite = mlist.end();
+
+    ++lit;
+    --lit;
+    while (lit != lite)
+    {
+        std::cout << *lit << std::endl;
+        ++lit;
+    }
+    std::list<int> ls(mlist);
+
+    return 0;
+}
